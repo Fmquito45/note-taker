3098c9e484bea48ba4dac7c542a0f40ad6ab2d39
Fmquito45 <fmquitox45@gmail.com>
2023-09-27 00:21:55 -0400

    imports modules needed and sets exports

---
+++ server.js
@@ -0,0 +1,13 @@
+//import modules needed
+const express = require('express');
+const apiRoute = require('./routes/apiRoute.js');
+const htmlRoute = require('./routes/htmlRoute.js');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+app.listen(POST, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
