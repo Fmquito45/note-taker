6a6cd3c843316be75874ef9e54198976953d7b91
Fmquito45 <fmquitox45@gmail.com>
2023-09-27 23:45:41 -0400

    adds uuid package to newPost for unique id on post request

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require("fs");
+const {v4: uuidv4} = require('uuid');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 router.get('/api/notes', async (req, res) => {
@@ -9,8 +10,16 @@ router.get('/api/notes', async (req, res) => {
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
 router.post('/api/notes', (req, res) => {
-
-})
+    const dbNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const newNotes = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    dbNotes.push(newNotes);
+    fs.writeFileSync("db/db.json", JSON.stringify(dbNotes));
+    res.json(dbNotes);
+});
 
 
 
