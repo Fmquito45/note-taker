ec5a4170d281281e01a51ff393e71e84b5bfcefb
Fmquito45 <fmquitox45@gmail.com>
2023-09-27 22:48:01 -0400

    adds fundamental code line for get and post request in apiRoute

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,6 +1,14 @@
 const router = require('express').router();
 
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/api/notes', async (req, res) => {
 
+});
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
+router.post('/api/notes', (req, res) => {
+
+})
 
 
 
