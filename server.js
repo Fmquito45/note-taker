35030d122695cd323bfd0f93e0e531f21ca642b0
Fmquito45 <fmquitox45@gmail.com>
2023-09-27 23:18:43 -0400

    adds middleware for api/html routes

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use(htmlRoute);
+app.use(apiRoute);
+
 
 
 app.listen(POST, () => {
