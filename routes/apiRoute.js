b8edb5c4d74cff38d622df80c8ba0de95b27687c
Fmquito45 <fmquitox45@gmail.com>
2023-09-27 23:14:52 -0400

    adds get request for api route

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -2,7 +2,8 @@ const router = require('express').router();
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 router.get('/api/notes', async (req, res) => {
-
+    const dbNotes = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    res.json(dbNotes);
 });
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
